1752d4752ca7c4728e798e94b2364e108b57b5f9
rpalacio93 <53068577+rpalacio93@users.noreply.github.com>
2020-07-02 14:03:19 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,11 +1,11 @@
 // Select color input
-var pixelColor = document.getElementById('colorPicker');
+let pixelColor = document.getElementById('colorPicker');
 
 // Select size input
-var grid = document.getElementById('pixelCanvas');
-var gridSize = document.getElementById('sizePicker');
-var gridHeight = document.getElementById('inputHeight');
-var gridWidth = document.getElementById('inputWidth');
+let grid = document.getElementById('pixelCanvas');
+let gridSize = document.getElementById('sizePicker');
+let gridHeight = document.getElementById('inputHeight');
+let gridWidth = document.getElementById('inputWidth');
 
 // When size is submitted by the user, call makeGrid()
 gridSize.addEventListener('submit', (event) => {
@@ -13,13 +13,15 @@ gridSize.addEventListener('submit', (event) => {
   makeGrid(gridHeight.value, gridWidth.value);
 });
 
-// Your code goes here!
+// Create grid depending on user input
 function makeGrid(height, width){
+  // Resets grid
   grid.innerHTML = "";
   for (let x = 0; x <= height - 1; x++){
     let row = grid.insertRow(x);
     for (let y = 0; y <= width - 1; y++){
       let cell = row.insertCell(y);
+      // Allows the user to color a specific cell
       cell.addEventListener('click', (event) => {
         cell.style.backgroundColor = pixelColor.value;
       });
